82e158f41c07e45785bdbf00ebf6d5ac2c80dc54
oreak <ore082@students.cs.ubc.ca>
2026-03-18 23:45:59 -0700

    functioning web job

--- better-reads/frontend/src/components/Home/Header.jsx
+++ better-reads/frontend/src/components/Home/Header.jsx
@@ -66,7 +66,7 @@ const Header = ({ userAvatar }) => {
             >
               <MenuItem component={RouterLink} to="/" onClick={handleMenuClose}>Search</MenuItem>
               <MenuItem component={RouterLink} to="/recommendations" onClick={handleMenuClose}>Recommendations</MenuItem>
-              <MenuItem component={RouterLink} to="/nlpsearch" onClick={handleMenuClose}>AI Match</MenuItem>
+             
               <MenuItem component={RouterLink} to="/profile" onClick={handleMenuClose}>Profile</MenuItem>
             </Menu>
           </>
